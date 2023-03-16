ea7f61d43784480b202e303bf645e43357b81e4f
Tom SIOUAN <tomsiouan@icloud.com>
2023-03-16 09:33:01 +0100

    update

--- src/model/HolePawnPot.java
+++ src/model/HolePawnPot.java
@@ -4,7 +4,7 @@ import boardifier.model.GameStageModel;
 import boardifier.model.GridElement;
 
 public class HolePawnPot extends GridElement {
-    public static final int PAWNS_IN_POT = 5;
+    public static final int PAWNS_IN_POT = 9;
     public HolePawnPot(int x, int y, GameStageModel gameStageModel) {
         // call the super-constructor to create a 4x1 grid, named "pawnpot", and in x,y in space
         super("pawnpot", x, y, PAWNS_IN_POT, 1, gameStageModel);
