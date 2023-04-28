a96378f007ab1eb405533f3e270e3b0638e4887e
Halm Robin <robin.halm68@gmail.com>
2023-04-28 22:28:23 +0200

    One FIXME fixed.

--- src/control/MerelleDecider.java
+++ src/control/MerelleDecider.java
@@ -154,13 +154,12 @@ public class MerelleDecider extends Decider {
                 {{3, 3}, {}, {}, {3, 1}, {}, {}, {3, 3}},
                 {{}, {2, 2}, {}, {2, 1}, {}, {2, 2}, {}},
                 {{}, {}, {1, 1}, {1, 1}, {1, 1}, {}, {}},
-                {{1, 3}, {1, 2}, {1, 1}, {1, 1}, {1, 2}, {1, 3}},
+                {{1, 3}, {1, 2}, {1, 1}, {}, {1, 1}, {1, 2}, {1, 3}},
                 {{}, {}, {1, 1}, {1, 1}, {1, 1}, {}, {}},
                 {{}, {2, 2}, {}, {2, 1}, {}, {2, 2}, {}},
                 {{3, 3}, {}, {}, {3, 1}, {}, {}, {3, 3}}
         };
 
-        // FIXME Index 0 out of bounds for length 0 in jumpTable
         int jumpX = jumpTable[point.y][point.x][0];
         int jumpY = jumpTable[point.y][point.x][1];
 
