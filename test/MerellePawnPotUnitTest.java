3b10c9152b981e5f4b43b7ae0788fa42bc78e52c
Halm Robin <robin.halm68@gmail.com>
2023-04-29 21:31:55 +0200

    Addition of two java class for test     - MerelleControllerUnitTest     - MerellePawnPotUnitTest

---
+++ test/MerellePawnPotUnitTest.java
@@ -0,0 +1,22 @@
+import boardifier.model.GameStageModel;
+import model.MerellePawnPot;
+import model.Pawn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class MerellePawnPotUnitTest {
+    @Test
+    public void testIsEmpty(){
+        GameStageModel gameStage = Mockito.mock(GameStageModel.class);
+        MerellePawnPot merellePawnPot = new MerellePawnPot(0,0,gameStage);
+
+        Assertions.assertTrue(merellePawnPot.isEmpty());
+
+        Pawn pawn = Mockito.mock(Pawn.class);
+        merellePawnPot.putElement(pawn, 0,0);
+
+        Assertions.assertFalse(merellePawnPot.isEmpty());
+    }
+}
