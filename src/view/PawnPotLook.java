a7ed51d11ab9a6e06202e4c00d49f74a304c14c0
Halm Robin <robin.halm68@gmail.com>
2023-05-24 08:55:42 +0200

    JavaFx adaptation (inProgress)

--- src/view/PawnPotLook.java
+++ src/view/PawnPotLook.java
@@ -1,43 +1,33 @@
 package view;
 
-import boardifier.model.GridElement;
+import boardifier.model.GameElement;
 import boardifier.view.GridLook;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
-//public class PawnPotLook extends GridLook {
-//
-//    public PawnPotLook(int cellWidth, int cellHeight, GridElement gridElement) {
-//        super(cellWidth, cellHeight, gridElement, -1, false);
-//    }
-//
-//    protected void createShape() {
-//        // draw cells
-//        GridElement gridElement = (GridElement) element;
-//        int nbRows = gridElement.getNbRows();
-//        // start by drawing the border of each cell, which will be change after
-//        for (int i = 0; i < nbRows; i++) {
-//            //top-left corner
-//            shape[i * cellHeight][0] = "\u250C";
-//            // top-right corner
-//            shape[i * cellHeight][cellWidth] = "\u2510";
-//            //bottom-left corner
-//            shape[(i + 1) * cellHeight][0] = "\u2514";
-//            // bottom-right corner
-//            shape[(i + 1) * cellHeight][cellWidth] = "\u2518";
-//
-//            for (int k = 1; k < cellWidth; k++) {
-//                shape[i * cellHeight][k] = "\u2500";
-//                shape[(i + 1) * cellHeight][k] = "\u2500";
-//            }
-//            // draw left & righ vertical lines
-//            for (int k = 1; k < cellHeight; k++) {
-//                shape[i * cellHeight + k][0] = "\u2502";
-//                shape[i * cellHeight + k][cellWidth] = "\u2502";
-//            }
-//        }
-//        // change intersections on first & last vert. border
-//        for (int i = 1; i < nbRows; i++) {
-//            shape[i * cellHeight][0] = "\u251C";
-//            shape[i * cellHeight][cellWidth] = "\u2524";
-//        }
-//    }
-//}
+public class PawnPotLook extends GridLook {
+
+    // the array of rectangle composing the grid
+    private Rectangle[] cells;
+
+    public PawnPotLook(int width, int height, GameElement element) {
+        super(width, height, (width-20), (width-20), 10, "0X000000", element);
+        cells = new Rectangle[4];
+        // create the rectangles.
+        for(int i=0;i<4;i++) {
+            cells[i] = new Rectangle(cellWidth, cellHeight, Color.WHITE);
+            cells[i].setStrokeWidth(3);
+            cells[i].setStrokeMiterLimit(10);
+            cells[i].setStrokeType(StrokeType.CENTERED);
+            cells[i].setStroke(Color.valueOf("0x333333"));
+            cells[i].setX(borderWidth);
+            cells[i].setY(i*cellHeight+borderWidth);
+            addShape(cells[i]);
+        }
+    }
+
+    @Override
+    public void onChange() {
+    }
+}
