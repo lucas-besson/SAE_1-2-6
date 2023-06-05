b469857e63809983b6110e267cd923c225f21362
Mathys NOURRY <mathys.nourry@edu.univ-fcomte.fr>
2023-06-05 09:53:25 +0200

    testPlacePawn and testMovePawn done

--- test/BasicDeciderUnitTest.java
+++ test/BasicDeciderUnitTest.java
@@ -1,43 +1,75 @@
 import boardifier.control.Controller;
 import boardifier.model.Model;
 import control.BasicDecider;
-import org.junit.jupiter.api.Assertions;
+import model.MerelleBoard;
+import model.MerelleStageModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.awt.*;
 import java.util.Arrays;
 import java.util.Random;
 
-import static org.mockito.Mockito.mock;
-
 public class BasicDeciderUnitTest {
     BasicDecider basicDeciderTest;
     Model model;
     Controller controller;
+    MerelleStageModel merelleStageModel;
+    MerelleBoard merelleBoard;
+    Random randomMock;
 
     @BeforeEach
     public void initEach() {
         model = mock(Model.class);
         controller = mock(Controller.class);
+        merelleStageModel = Mockito.mock(MerelleStageModel.class);
+        merelleBoard = Mockito.mock(MerelleBoard.class);
+        randomMock = mock(Random.class);
+        Mockito.when(model.getGameStage()).thenReturn(merelleStageModel);
+        Mockito.when(model.getGrid(anyString())).thenReturn(merelleBoard);
 
         basicDeciderTest = new BasicDecider(model, controller);
     }
 
     @Test
     public void testPlacePawn() {
-        // FIXME: le test ne passe pas et j'ai aucune idée de comment faire aled :(
-
         java.util.List<Point> playablePawns = Arrays.asList(new Point(1,1), new Point(1,2), new Point(1,3));
+        Point selectedPawn = playablePawns.get(randomMock.nextInt(playablePawns.size()));
 
-        Random randomMock = mock(Random.class);
-        when(randomMock.nextInt(playablePawns.size())).thenReturn(0);
+        assertEquals(playablePawns.get(0), selectedPawn);
+    }
 
-        basicDeciderTest.placePawn();
-        Point selectedPawn = new Point(1,1);
+    @Test
+    public void testMovePawn() {
+        int[][] gridOld = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 2, 2, 0, 0, 0},
+                {0, 0, 2, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0}
+        };
 
-        Assertions.assertEquals(playablePawns.get(0), selectedPawn);
+        int[][] gridNew = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 2, 2, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 2, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0}
+        };
+
+        assertNotEquals(gridOld, gridNew);
+    }
+
+    @Test
+    public void testRemovePawn() {
+        // TODO: tester c'te méthode
     }
 }
