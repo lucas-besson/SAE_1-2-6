df7df3ff147eaf057dfffa1cef1de4169d368551
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-06-06 18:00:32 +0200

    Relative path for Images instead of Abs path

--- src/view/Images.java
+++ src/view/Images.java
@@ -4,6 +4,6 @@ import javafx.scene.image.Image;
 
 class Images {
     public static Image loadImage(String relPath) {
-        return new Image(String.valueOf(Images.class.getResource("..\\" + relPath)));
+        return new Image(String.valueOf(Images.class.getResource("../" + relPath)));
     }
 }
