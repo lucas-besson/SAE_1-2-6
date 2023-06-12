0d5faa2ba2392cf2df0e06c2785cae852e8b077f
Halm Robin <robin.halm68@gmail.com>
2023-06-13 00:40:57 +0200

    Test addition for callBack and refactoring test code

--- test/MerelleBoardUnitTest.java
+++ test/MerelleBoardUnitTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 public class MerelleBoardUnitTest {
 
     private MerelleBoard board;
-    private boolean[][] wantedReachableCells = new boolean[][]{
+    private final boolean[][] wantedReachableCells = new boolean[][]{
             {false, false, false, false, false, false, false},
             {false, false, false, false, false, false, false},
             {false, false, false, false, false, false, false},
@@ -22,56 +25,56 @@ public class MerelleBoardUnitTest {
 
     @BeforeEach
     public void initEach() {
-        GameStageModel gameStage = Mockito.mock(GameStageModel.class);
+        GameStageModel gameStage = mock(GameStageModel.class);
         board = new MerelleBoard(0, 0, gameStage);
     }
 
     @Test
     public void testisActiveCell() {
-        Assertions.assertTrue(MerelleBoard.isActiveCell(0, 0));
-        Assertions.assertFalse(MerelleBoard.isActiveCell(0, 4));
-        Assertions.assertFalse(MerelleBoard.isActiveCell(7, 7));
+        assertTrue(MerelleBoard.isActiveCell(0, 0));
+        assertFalse(MerelleBoard.isActiveCell(0, 4));
+        assertFalse(MerelleBoard.isActiveCell(7, 7));
     }
 
 
     @Test
     public void testgetPawnABF() {
-        Assertions.assertNull(board.getPawn(1, 1));
+        assertNull(board.getPawn(1, 1));
     }
 
     @Test
     public void testgetPawnABCF() {
-        Pawn pawn = Mockito.mock(Pawn.class);
+        Pawn pawn = mock(Pawn.class);
         board.putElement(pawn, 0, 0);
-        Mockito.when(pawn.getColor()).thenReturn(1);
-        Mockito.when(pawn.getNumber()).thenReturn(1);
-        Assertions.assertNull(board.getPawn(3, 3));
+        when(pawn.getColor()).thenReturn(1);
+        when(pawn.getNumber()).thenReturn(1);
+        assertNull(board.getPawn(3, 3));
     }
 
     @Test
     public void testgetPawnABCDF() {
-        Pawn pawn = Mockito.mock(Pawn.class);
+        Pawn pawn = mock(Pawn.class);
         board.putElement(pawn, 0, 0);
-        Mockito.when(pawn.getColor()).thenReturn(1);
-        Mockito.when(pawn.getNumber()).thenReturn(1);
-        Assertions.assertNull(board.getPawn(3, 1));
+        when(pawn.getColor()).thenReturn(1);
+        when(pawn.getNumber()).thenReturn(1);
+        assertNull(board.getPawn(3, 1));
     }
 
 
     @Test
     public void testgetPawnABCDE() {
-        Pawn pawn = Mockito.mock(Pawn.class);
+        Pawn pawn = mock(Pawn.class);
         board.putElement(pawn, 0, 0);
-        Mockito.when(pawn.getColor()).thenReturn(3);
-        Mockito.when(pawn.getNumber()).thenReturn(3);
-        Assertions.assertEquals((board.getPawn(3, 3)), pawn);
+        when(pawn.getColor()).thenReturn(3);
+        when(pawn.getNumber()).thenReturn(3);
+        assertEquals((board.getPawn(3, 3)), pawn);
     }
 
     @Test
     public void testMillsChecker() {
-        Pawn p1 = Mockito.mock(Pawn.class);
-        Pawn p2 = Mockito.mock(Pawn.class);
-        Pawn p3 = Mockito.mock(Pawn.class);
+        Pawn p1 = mock(Pawn.class);
+        Pawn p2 = mock(Pawn.class);
+        Pawn p3 = mock(Pawn.class);
 
         int[][] mill1 = MerelleBoard.MILLS[0];
         int[][] mill2 = MerelleBoard.MILLS[1];
@@ -79,46 +82,46 @@ public class MerelleBoardUnitTest {
         // Only 2 pawn, not 3
         board.putElement(p1, mill1[0][0], mill1[0][1]);
         board.putElement(p2, mill1[1][0], mill1[1][1]);
-        Mockito.when(p1.getColor()).thenReturn(1);
-        Mockito.when(p2.getColor()).thenReturn(1);
-        Assertions.assertFalse(board.millsChecker(1));
+        when(p1.getColor()).thenReturn(1);
+        when(p2.getColor()).thenReturn(1);
+        assertFalse(board.millsChecker(1));
 
         // 3 pawn but in the wrong place
         board.putElement(p3, mill2[2][0], mill2[2][1]);
-        Mockito.when(p3.getColor()).thenReturn(1);
-        Assertions.assertFalse(board.millsChecker(1));
+        when(p3.getColor()).thenReturn(1);
+        assertFalse(board.millsChecker(1));
 
         // 3 pawn int the right place but of the wrong color
         board.putElement(p3, mill1[2][0], mill1[2][1]);
-        Mockito.when(p3.getColor()).thenReturn(2);
-        Assertions.assertFalse(board.millsChecker(1));
+        when(p3.getColor()).thenReturn(2);
+        assertFalse(board.millsChecker(1));
 
         // 3 pawn int the right place of the right color
         board.putElement(p3, mill1[2][0], mill1[2][1]);
-        Mockito.when(p3.getColor()).thenReturn(1);
-        Assertions.assertTrue(board.millsChecker(1));
+        when(p3.getColor()).thenReturn(1);
+        assertTrue(board.millsChecker(1));
 
         // A new mill has been formed, the millUpdate is called
-        Mockito.verify(p1).setInAMill(Mockito.anyBoolean());
-        Mockito.when(p1.isInAMill()).thenReturn(true);
-        Mockito.verify(p2).setInAMill(Mockito.anyBoolean());
-        Mockito.when(p2.isInAMill()).thenReturn(true);
-        Mockito.verify(p3).setInAMill(Mockito.anyBoolean());
-        Mockito.when(p3.isInAMill()).thenReturn(true);
+        verify(p1).setInAMill(anyBoolean());
+        when(p1.isInAMill()).thenReturn(true);
+        verify(p2).setInAMill(anyBoolean());
+        when(p2.isInAMill()).thenReturn(true);
+        verify(p3).setInAMill(anyBoolean());
+        when(p3.isInAMill()).thenReturn(true);
 
         // The mill has already been formed, millsChecker cannot return true a second time
-        Assertions.assertFalse(board.millsChecker(1));
+        assertFalse(board.millsChecker(1));
 
         // if the pawn have been mooed back and forward, then it is a new mill -> return true
-        Mockito.when(p1.isInAMill()).thenReturn(true);
-        Mockito.when(p2.isInAMill()).thenReturn(true);
-        Mockito.when(p3.isInAMill()).thenReturn(false);
+        when(p1.isInAMill()).thenReturn(true);
+        when(p2.isInAMill()).thenReturn(true);
+        when(p3.isInAMill()).thenReturn(false);
 
-        Assertions.assertTrue(board.millsChecker(1));
+        assertTrue(board.millsChecker(1));
 
-        Mockito.verify(p1, Mockito.times(2)).setInAMill(Mockito.anyBoolean());
-        Mockito.verify(p2, Mockito.times(2)).setInAMill(Mockito.anyBoolean());
-        Mockito.verify(p3, Mockito.times(2)).setInAMill(Mockito.anyBoolean());
+        verify(p1, times(2)).setInAMill(anyBoolean());
+        verify(p2, times(2)).setInAMill(anyBoolean());
+        verify(p3, times(2)).setInAMill(anyBoolean());
 
     }
 
@@ -126,39 +129,39 @@ public class MerelleBoardUnitTest {
     public void testavailableMoves() {
 
         // No pawn is in the grid
-        Assertions.assertEquals(24, board.availableMoves(0, 1));
-        Assertions.assertEquals(0, board.availableMoves(0, 2));
+        assertEquals(24, board.availableMoves(0, 1));
+        assertEquals(0, board.availableMoves(0, 2));
 
         // 1 pawn, in the corner, in the grid
-        Pawn p1 = Mockito.mock(Pawn.class);
+        Pawn p1 = mock(Pawn.class);
         board.putElement(p1, 0, 0);
-        Mockito.when(p1.getColor()).thenReturn(0);
-        Mockito.when(p1.getCol()).thenReturn(0);
-        Mockito.when(p1.getRow()).thenReturn(0);
-        Assertions.assertEquals(23, board.availableMoves(0, 1));
-        Assertions.assertEquals(2, board.availableMoves(0, 2));
+        when(p1.getColor()).thenReturn(0);
+        when(p1.getCol()).thenReturn(0);
+        when(p1.getRow()).thenReturn(0);
+        assertEquals(23, board.availableMoves(0, 1));
+        assertEquals(2, board.availableMoves(0, 2));
 
         // 2 pawn, in the corner, next to each other, in the grid
-        Pawn p2 = Mockito.mock(Pawn.class);
+        Pawn p2 = mock(Pawn.class);
         board.putElement(p2, 3, 0);
-        Mockito.when(p2.getColor()).thenReturn(0);
-        Mockito.when(p2.getCol()).thenReturn(0);
-        Mockito.when(p2.getRow()).thenReturn(3);
-        Assertions.assertEquals(22, board.availableMoves(0, 1));
-        Assertions.assertEquals(3, board.availableMoves(0, 2));
+        when(p2.getColor()).thenReturn(0);
+        when(p2.getCol()).thenReturn(0);
+        when(p2.getRow()).thenReturn(3);
+        assertEquals(22, board.availableMoves(0, 1));
+        assertEquals(3, board.availableMoves(0, 2));
 
         // 3 pawn, two in the corner next to each other, on close to the middle, in the grid
-        Pawn p3 = Mockito.mock(Pawn.class);
+        Pawn p3 = mock(Pawn.class);
         board.putElement(p3, 3, 2);
-        Mockito.when(p3.getColor()).thenReturn(0);
-        Mockito.when(p3.getCol()).thenReturn(2);
-        Mockito.when(p3.getRow()).thenReturn(3);
-        Assertions.assertEquals(21, board.availableMoves(0, 1));
-        Assertions.assertEquals(6, board.availableMoves(0, 2));
+        when(p3.getColor()).thenReturn(0);
+        when(p3.getCol()).thenReturn(2);
+        when(p3.getRow()).thenReturn(3);
+        assertEquals(21, board.availableMoves(0, 1));
+        assertEquals(6, board.availableMoves(0, 2));
 
         // All the pawn are encircle
-        Pawn p4 = Mockito.mock(Pawn.class);
-        Mockito.when(p4.getColor()).thenReturn(1);
+        Pawn p4 = mock(Pawn.class);
+        when(p4.getColor()).thenReturn(1);
         board.putElement(p4, 0, 3);
         board.putElement(p4, 6, 0);
         board.putElement(p4, 3, 1);
@@ -166,7 +169,7 @@ public class MerelleBoardUnitTest {
         board.putElement(p4, 4, 2);
         board.putElement(p4, 3, 1);
 
-        Assertions.assertEquals(0, board.availableMoves(0, 2));
+        assertEquals(0, board.availableMoves(0, 2));
     }
 
     private void resetReachableCellsTable(boolean[][] table, boolean state) {
@@ -177,30 +180,30 @@ public class MerelleBoardUnitTest {
 
     @Test
     public void testSetValidCells() {
-        Pawn pawn = Mockito.mock(Pawn.class);
+        Pawn pawn = mock(Pawn.class);
         resetReachableCellsTable(wantedReachableCells, true);
         board.setValidCells(pawn, 1);
-        Assertions.assertArrayEquals(wantedReachableCells, board.getReachableCells());
+        assertArrayEquals(wantedReachableCells, board.getReachableCells());
     }
 
     @Test
     public void testSetValidMillCells() {
 
         board.setValidMillCells(1);
-        Assertions.assertArrayEquals(wantedReachableCells, board.getReachableCells());
+        assertArrayEquals(wantedReachableCells, board.getReachableCells());
 
-        Pawn pawn2 = Mockito.mock(Pawn.class);
-        Mockito.when(pawn2.getColor()).thenReturn(0);
+        Pawn pawn2 = mock(Pawn.class);
+        when(pawn2.getColor()).thenReturn(0);
         board.putElement(pawn2, 0, 3);
         wantedReachableCells[0][3] = true;
         board.setValidMillCells(1);
-        Assertions.assertArrayEquals(wantedReachableCells, board.getReachableCells());
+        assertArrayEquals(wantedReachableCells, board.getReachableCells());
 
-        Pawn pawn3 = Mockito.mock(Pawn.class);
-        Mockito.when(pawn3.getColor()).thenReturn(1);
+        Pawn pawn3 = mock(Pawn.class);
+        when(pawn3.getColor()).thenReturn(1);
         board.putElement(pawn3, 1, 1);
         board.setValidMillCells(1);
-        Assertions.assertArrayEquals(wantedReachableCells, board.getReachableCells());
+        assertArrayEquals(wantedReachableCells, board.getReachableCells());
 
     }
 }
--- test/MerelleStageModelUnitTest.java
+++ test/MerelleStageModelUnitTest.java
@@ -4,33 +4,47 @@ import model.MerelleBoard;
 import model.MerellePawnPot;
 import model.MerelleStageModel;
 import model.Pawn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import view.PawnPotLook;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
 public class MerelleStageModelUnitTest {
+
+    private MerelleBoard merelleBoard;
+    private Model model;
+    private MerelleStageModel merelleStageModel;
+    private MerellePawnPot merellePawnPot;
+
+    @BeforeEach
+    public void beforeEach() {
+        merelleBoard = mock(MerelleBoard.class);
+        model = mock(Model.class);
+        merelleStageModel = new MerelleStageModel("test", model);
+        merellePawnPot = mock(MerellePawnPot.class);
+        when(model.getGameStage()).thenReturn(merelleStageModel);
+        merelleStageModel.setBoard(merelleBoard);
+        merelleStageModel.setBlackPot(merellePawnPot);
+    }
 
     @Test
     public void testisEndStage() {
-        MerelleBoard merelleBoard = Mockito.mock(MerelleBoard.class);
-        Model model = Mockito.mock(Model.class);
-        MerelleStageModel merelleStageModel = new MerelleStageModel("test", model);
-        MerellePawnPot merellePawnPot = Mockito.mock(MerellePawnPot.class);
-        //Mockito.when(model).thenReturn(merelleStageModel);
-        Mockito.when(model.getGameStage()).thenReturn(merelleStageModel);
-        merelleStageModel.setBoard(merelleBoard);
-        merelleStageModel.setBlackPot(merellePawnPot);
-//        merelleStageModel.isEndStage();
+
 
         //Premier if :
-        Mockito.when(model.getIdPlayer()).thenReturn(3);
-        Mockito.when(merelleBoard.availableMoves(Mockito.anyInt(), Mockito.anyInt())).thenReturn(0);
+        when(model.getIdPlayer()).thenReturn(3);
+        when(merelleBoard.availableMoves(anyInt(), anyInt())).thenReturn(0);
         merelleStageModel.isEndStage();
-        Mockito.verify(model, Mockito.times(2)).getIdPlayer();
-        Mockito.verify(model).setIdWinner(3);
+        verify(model, times(2)).getIdPlayer();
+        verify(model).setIdWinner(3);
         // Troisème IF :
-        Mockito.when(merelleBoard.availableMoves(Mockito.anyInt(), Mockito.anyInt())).thenReturn(2);
+        when(merelleBoard.availableMoves(anyInt(), anyInt())).thenReturn(2);
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
@@ -38,10 +52,10 @@ public class MerelleStageModelUnitTest {
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 1, merelleStageModel), merelleBoard, 0, 0);
-        Mockito.verify(model).setIdWinner(0);
+        verify(model).setIdWinner(0);
 
         // Deuxieme IF :
-        Mockito.when(merelleBoard.availableMoves(Mockito.anyInt(), Mockito.anyInt())).thenReturn(2);
+        when(merelleBoard.availableMoves(anyInt(), anyInt())).thenReturn(2);
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
@@ -49,8 +63,45 @@ public class MerelleStageModelUnitTest {
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
         merelleStageModel.removedFromGrid(new Pawn(1, 0, merelleStageModel), merelleBoard, 0, 0);
-        Mockito.verify(model).setIdWinner(1);
-
+        verify(model).setIdWinner(1);
     }
 
+    @Test
+    public void testCallBacks() {
+        Pawn pawn = mock(Pawn.class);
+
+        // onSelectionChange :
+        // skip the if because the selected list isn't empty
+        merelleStageModel.setSelected(pawn, true);
+        verify(merelleBoard).setValidCells(eq(pawn), anyInt());
+        // go through the if because the selected list is empty
+        merelleStageModel.setSelected(pawn, false);
+        verify(merelleBoard).resetReachableCells(false);
+
+        // onPutInGrid :
+        merelleStageModel.putInGrid(pawn, merelleBoard, 0, 1);
+        verify(pawn).setInAMill(false);
+        verify(pawn).setCol(1);
+        verify(pawn).setRow(0);
+
+        // onMoveInGrid :
+        merelleStageModel.movedInGrid(pawn, merelleBoard, 2, 3);
+        verify(pawn, times(2)).setInAMill(false);
+        verify(pawn).setCol(3);
+        verify(pawn).setRow(2);
+
+        // onRemoveFromGrid :
+        // if the board is the same as in the stageModel
+        merelleStageModel.putInGrid(pawn, mock(MerelleBoard.class), 2, 3);
+
+        // if player play black pawn
+        when(pawn.getColor()).thenReturn(0);
+        merelleStageModel.removedFromGrid(pawn, merelleBoard, 2, 3);
+        assertEquals(MerellePawnPot.PAWNS_IN_POT - 1, merelleStageModel.getBlackPawnsToPlay());
+
+        // if player play red pawn
+        when(pawn.getColor()).thenReturn(1);
+        merelleStageModel.removedFromGrid(pawn, merelleBoard, 2, 3);
+        assertEquals(MerellePawnPot.PAWNS_IN_POT - 1, merelleStageModel.getRedPawnsToPlay());
+    }
 }
