1fed20bd61df29f5b454a1841520e4537f4e9d4c
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-06-14 22:42:36 +0200

    Ajout des fenetres pour le menu "Help"

--- src/view/MerelleRootPane.java
+++ src/view/MerelleRootPane.java
@@ -5,6 +5,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class MerelleRootPane extends RootPane {
 
@@ -20,11 +21,13 @@ public class MerelleRootPane extends RootPane {
         width = 1080;
         height = 720;
         Rectangle frame = new Rectangle(width, height, Color.LIGHTGREY);
-        Text text = new Text("Playing to Nine Men's Morris");
+        Text text = new Text("Playing to Nine Men's Morris\n\n" + HelpStage.HelpText);
         text.setFont(new Font(15));
         text.setFill(Color.BLACK);
-        text.setX(10);
+        text.setX(20);
         text.setY(50);
+        text.setWrappingWidth(width - 40);
+        text.setTextAlignment(TextAlignment.JUSTIFY);
         // put shapes in the group
         group.getChildren().clear();
         group.getChildren().addAll(frame, text);
