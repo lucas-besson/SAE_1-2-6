ab3c4e8bd29336ae160abc00e796517128d092ae
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-06-15 20:23:30 +0200

    Améliorations apres passage dans SonarLint

--- src/view/HelpStage.java
+++ src/view/HelpStage.java
@@ -12,27 +12,33 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class HelpStage extends Stage {
-    static final String HelpText = "Nine Men's Morris is an ancient strategy board game that dates back to the Roman Empire and was popular throughout Europe during the Middle Ages. The game is played on a grid consisting of three concentric squares connected by lines. Here are the rules:\n" +
-            "\n\t1.\t\tThe game starts with an empty board. Each player has nine pieces, distinguished by color, such as red and black.\n" +
-            "\t2.\t\tPlayers take turns placing their pieces on the intersections of the lines. The goal is to form \"mills\", which are vertical or horizontal lines of three pieces of the same color. When a mill is formed, the player who created it can remove one of their opponent's pieces from the board (by clicking on it).\n" +
-            "\t3.\t\tAfter all the pieces have been placed on the board, players take turns moving their pieces along the lines to adjacent empty intersections (by clicking on the pawn and the wanted destination). The pieces can move in any direction along the lines, but they cannot jump over other pieces.\n" +
-            "\t4.\t\tWhen a player's move creates a mill (three pieces in a row), they can remove one of their opponent's pieces from the board.\n" +
-            "\t5.\t\tThe game continues until one player has only two pieces left or is unable to make a legal move. In the latter case, the other player wins.\n" +
-            "\t6.\t\tThe game ends when one player captures enough of their opponent's pieces to reduce them to two, or when a player is unable to make a legal move.\n" +
-            "\nStrategic thinking in Nine Men's Morris revolves around creating mills while simultaneously blocking your opponent's moves and preventing them from forming mills. The ability to anticipate and disrupt your opponent's plans is crucial for success in this game.\n" +
-            "It's up to you!";
+    static final String HelpText = """
+            Nine Men's Morris is an ancient strategy board game that dates back to the Roman Empire and was popular throughout Europe during the Middle Ages. The game is played on a grid consisting of three concentric squares connected by lines. Here are the rules:
+            
+            \t1.\t\tThe game starts with an empty board. Each player has nine pieces, distinguished by color, such as red and black.
+            \t2.\t\tPlayers take turns placing their pieces on the intersections of the lines. The goal is to form "mills", which are vertical or horizontal lines of three pieces of the same color. When a mill is formed, the player who created it can remove one of their opponent's pieces from the board (by clicking on it).
+            \t3.\t\tAfter all the pieces have been placed on the board, players take turns moving their pieces along the lines to adjacent empty intersections (by clicking on the pawn and the wanted destination). The pieces can move in any direction along the lines, but they cannot jump over other pieces.
+            \t4.\t\tWhen a player's move creates a mill (three pieces in a row), they can remove one of their opponent's pieces from the board.
+            \t5.\t\tThe game continues until one player has only two pieces left or is unable to make a legal move. In the latter case, the other player wins.
+            \t6.\t\tThe game ends when one player captures enough of their opponent's pieces to reduce them to two, or when a player is unable to make a legal move.
+            
+            Strategic thinking in Nine Men's Morris revolves around creating mills while simultaneously blocking your opponent's moves and preventing them from forming mills. The ability to anticipate and disrupt your opponent's plans is crucial for success in this game.
+            It's up to you!""";
 
-    static final String Credits = "Developed with ❤ by :\n" +
-            "   Lucas BESSON\n" +
-            "   Nathan BOSCHI\n" +
-            "   Robin HALM\n" +
-            "   Mathys NOURRY\n" +
-            "   Tom SIOUAN\n" +
-            "\nUsing :\n" +
-            "   - JAVA Language\n" +
-            "   - JavaFX Framework\n" +
-            "   - Boardifier Framework\n" +
-            "\nOur github repository : [https://github.com/lucas-besson/SAE_1-2-6]";
+    static final String Credits = """
+            Developed with ❤ by :
+            \tLucas BESSON
+            \tNathan BOSCHI
+            \tRobin HALM
+            \tMathys NOURRY
+            \tTom SIOUAN
+               
+            Using :
+            \t- JAVA Language
+            \t- JavaFX Framework
+            \t- Boardifier Framework
+               
+            Our github repository : [https://github.com/lucas-besson/SAE_1-2-6]""";
     private FlowPane mainPane;
 
     public HelpStage(TypeOfHelp type) {
