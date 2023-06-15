8bd40d229b083819ab76507df2a34733a104006e
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-06-15 09:15:16 +0200

    Help text

--- src/view/HelpStage.java
+++ src/view/HelpStage.java
@@ -6,27 +6,33 @@ import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class HelpStage extends Stage {
-    static final String HelpText = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit neque et elit suscipit convallis vel accumsan odio. Donec et ligula tempus, tincidunt mauris vitae, tincidunt leo. Vivamus bibendum quam in nibh sollicitudin, id viverra tortor cursus. Phasellus semper diam eget tristique rutrum. Donec nec nisi libero. Sed vitae dolor ante. Curabitur congue est ac cursus suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis aliquet magna eu bibendum tempus. Praesent sed ornare diam, at blandit lacus. Suspendisse eget tempus orci, sed tincidunt turpis.\n" +
-            "\n" +
-            "Sed suscipit, libero ac pretium porta, lacus leo pellentesque massa, cursus tincidunt lectus mauris eget erat. Integer et rhoncus massa. Sed in arcu vel nunc convallis commodo. Phasellus non condimentum dolor, sit amet mollis mi. Donec vel volutpat diam. Morbi scelerisque placerat leo ac efficitur. Vestibulum interdum, sem et sagittis porta, lorem purus ullamcorper risus, id laoreet purus sapien in sem. Mauris massa risus, varius vel blandit at, tincidunt id mauris. Nunc faucibus tellus non magna venenatis pharetra. Donec ut faucibus odio. Donec sed elit justo. Duis finibus tellus sed ex maximus tempor. Donec feugiat risus enim, eu pretium sapien congue eu. Integer nec faucibus nibh, cursus aliquet lorem. Nullam ac metus a est euismod venenatis.\n" +
-            "\n" +
-            "Duis elementum purus dapibus turpis pellentesque euismod. Proin molestie nisi nec nunc laoreet blandit. In consectetur mauris elementum, bibendum enim sed, dapibus lacus. Curabitur rhoncus leo a varius vestibulum. Duis sed justo ornare nunc rhoncus volutpat eu nec lorem. Vestibulum vitae facilisis dolor. Ut lacinia felis ac metus malesuada congue scelerisque vel nisl. Mauris rutrum nisi eget lobortis cursus. Mauris rutrum turpis urna, nec imperdiet felis euismod at. Suspendisse quis aliquet justo. Sed mattis augue id cursus semper. ";
+    static final String HelpText = "Nine Men's Morris is an ancient strategy board game that dates back to the Roman Empire and was popular throughout Europe during the Middle Ages. The game is played on a grid consisting of three concentric squares connected by lines. Here are the rules:\n" +
+            "\n\t1.\t\tThe game starts with an empty board. Each player has nine pieces, distinguished by color, such as red and black.\n" +
+            "\t2.\t\tPlayers take turns placing their pieces on the intersections of the lines. The goal is to form \"mills\", which are vertical or horizontal lines of three pieces of the same color. When a mill is formed, the player who created it can remove one of their opponent's pieces from the board (by clicking on it).\n" +
+            "\t3.\t\tAfter all the pieces have been placed on the board, players take turns moving their pieces along the lines to adjacent empty intersections (by clicking on the pawn and after the place to move that pawn). The pieces can move in any direction along the lines, but they cannot jump over other pieces.\n" +
+            "\t4.\t\tWhen a player's move creates a mill (three pieces in a row), they can remove one of their opponent's pieces from the board.\n" +
+            "\t5.\t\tThe game continues until one player has only two pieces left or is unable to make a legal move. In the latter case, the other player wins.\n" +
+            "\t6.\t\tThe game ends when one player captures enough of their opponent's pieces to reduce them to two, or when a player is unable to make a legal move.\n" +
+            "\nStrategic thinking in Nine Men's Morris revolves around creating mills while simultaneously blocking your opponent's moves and preventing them from forming mills. The ability to anticipate and disrupt your opponent's plans is crucial for success in this game.\n" +
+            "It's up to you!";
 
-    static final String Credits = "Developed with heart by :\n" +
+    static final String Credits = "Developed with ❤ by :\n" +
             "   Lucas BESSON\n" +
             "   Nathan BOSCHI\n" +
             "   Robin HALM\n" +
             "   Mathys NOURRY\n" +
             "   Tom SIOUAN\n" +
-            "\nEn utilisant :\n" +
-            "   - Language JAVA\n" +
-            "   - Framework JavaFX\n" +
-            "   - Framework Boardifier";
+            "\nUsing :\n" +
+            "   - JAVA Language\n" +
+            "   - JavaFX Framework\n" +
+            "   - Boardifier Framework\n" +
+            "\nOur github repository : [https://github.com/lucas-besson/SAE_1-2-6]";
     private FlowPane mainPane;
 
     public HelpStage(TypeOfHelp type) {
@@ -41,6 +47,7 @@ public class HelpStage extends Stage {
         text.setFill(Color.BLACK);
         text.setWrappingWidth(700);
         text.setTextAlignment(TextAlignment.JUSTIFY);
+        text.setFontSmoothingType(FontSmoothingType.LCD);
 
         mainPane = new FlowPane();
         mainPane.getChildren().add(text);
@@ -55,6 +62,6 @@ public class HelpStage extends Stage {
     }
 
     public enum TypeOfHelp {
-        HOW_TO_PLAY, CREDITS
+        HOW_TO_PLAY, CREDITS;
     }
 }
